1ba0906f7aec7f685d76ecb61e63b715ef4240e5
MarkBen0 <55602832+MarkBen0@users.noreply.github.com>
2024-01-26 15:56:45 -0800

    Jump and Ground

---
+++ Laugh You Lose/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Ray2D ray;
+    Rigidbody2D rb;
+    public float m_Thrust = 20f;
+    public float JumpRay = 100f;
+    public LayerMask floorLayer;
+    Collider2D floor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ray = new Ray2D(transform.position, -transform.up);
+        
+        bool CanJump = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.down), JumpRay, floorLayer);
+        if(CanJump)
+        {
+            Debug.Log("here");
+        }
+        if (Input.GetButtonDown("Jump") && CanJump)
+        {
+            //rb.AddForce(transform.up * m_Thrust);
+            rb.velocityY = m_Thrust;
+        }
+    }
+}
