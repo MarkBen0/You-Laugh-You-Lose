1150999a2b7cf57ca6ffa1b20945d6f238c153e3
MarkBen0 <55602832+MarkBen0@users.noreply.github.com>
2024-01-27 23:32:22 -0800

    Added Start Screen
    also health display and score

--- Laugh You Lose/Assets/GameManager.cs
+++ Laugh You Lose/Assets/GameManager.cs
@@ -1,24 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public float speed;
+    [SerializeField] private TextMeshProUGUI ScoreDisplay;
+    [SerializeField] private int score = 0;
+    [SerializeField] private float TimeMultuplier = 10;
+    [SerializeField] private int ScoreMultuplier = 1;
+    [SerializeField] private float ToSpeed;
+    [SerializeField] private float TopSpeed;
+    [SerializeField] private float SpeedPower;
+    [SerializeField] private GameObject StartButton;
+    [SerializeField] private GameObject RestartButton;
+    [SerializeField] private GameObject Image;
 
     void Awake()
     {
         Instance = this;
     }
 
-    void Start()
+    public void GameStart()
     {
-
+        Time.timeScale = 1;
+        StartCoroutine(ScoreCoroutine());
+        StartButton.active = false;
+        Image.active = false;
     }
-    
-    void Update()
-    {
 
+    void FixedUpdate()
+    {
+        speed = Mathf.Pow(Time.fixedTime/ToSpeed, 1f/SpeedPower)*TopSpeed;
+    }
+
+    IEnumerator ScoreCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1f / TimeMultuplier);
+            score += ScoreMultuplier;
+            ScoreDisplay.text = score.ToString();
+        }        
     }
 }
